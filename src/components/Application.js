db25d5e0a0b5152ee2fb6e6e504c5abb67688f1b
Elroy Hui <huielroy@hotmail.com>
2022-04-22 12:05:23 -0400

    +implemented make API Get to 8001 proxy

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,9 @@
-import React, { useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "components/DayList.js";
 import Appointment from "components/Appointment/index.js";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   1: {
@@ -62,11 +45,20 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+  const [day, setDay] = useState(["Monday"]);
+  const [days, setDays] = useState([]);
 
   const appMapped = Object.values(appointments).map((appointment) => {
     return <Appointment key={appointment.id} {...appointment} />;
   });
+
+  useEffect(() => {
+    const testURL = `/api/days`;
+    axios.get(testURL).then((response) => {
+      console.log(response.data);
+      setDays([...response.data]);
+    });
+  }, []);
 
   return (
     <main className="layout">
