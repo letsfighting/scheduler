969b80c787bb3a6154eed48a484b5d296d65a085
Elroy Hui <huielroy@hotmail.com>
2022-04-22 12:35:06 -0400

    +selector getAppointmentsForDay

---
+++ src/helpers/selectors.js
@@ -0,0 +1,23 @@
+export function getAppointmentsForDay(state, day) {
+  const stepOne = () => {
+    for (const eachDay of state["days"]) {
+      if (eachDay["name"] === day) {
+        return eachDay.appointments;
+      }
+    }
+  };
+
+  const resStepOne = stepOne();
+  let appArray = [];
+
+  if (resStepOne !== undefined) {
+    for (const app of resStepOne) {
+      if (state["appointments"][app]) {
+        appArray.push(state["appointments"][app]);
+      }
+    }
+  }
+
+  console.log("final array: ", appArray);
+  return appArray;
+}
